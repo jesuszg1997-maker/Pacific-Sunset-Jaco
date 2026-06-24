a62a151f10e8070b6a58686e223d2e5064290cb8
Chuz <jesuszg1997@gmail.com>
2026-06-23 19:35:10 -0600

    updating website title

--- homepage.py
+++ homepage.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Pacific Sunset Jaco",
+    page_title="White Witch Apartment",
     page_icon="🌅",
     layout="wide",
     initial_sidebar_state="collapsed",
